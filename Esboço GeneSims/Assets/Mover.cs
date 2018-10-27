37c236b56d18fd2f076e633b301fee4a8e8e42c0
joao9aulo <joao9aulo@gmail.com>
2018-10-27 13:05:14 -0300

    estudo joints

---
+++ Esboço GeneSims/Assets/Mover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float force;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, -force * Time.deltaTime);
+    }
+}
